f7fc8cd78cfad048a3e4f9b2a37fae3ef11f7b98
maxiong88 <784972981@qq.com>
2018-07-04 21:33:30 +0800

    gengxin

--- api/articlelist.php
+++ api/articlelist.php
@@ -12,11 +12,74 @@ if($page == '1'){
             "id" : 1,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 2,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 1,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 2,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 1,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 2,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 1,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 2,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 1,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 2,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 1,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 2,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 1,
@@ -27,66 +90,19 @@ if($page == '1'){
             "id" : 2,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 1,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 2,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 1,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 2,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 1,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 2,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 1,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 2,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 1,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 2,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 1,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 2,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         }]
     }';
@@ -101,16 +117,43 @@ if($page == '1'){
             "id" : 1,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 2,
             "info_msg" : "1设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 1,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 2,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 1,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 2,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
+            "info_time" : "2090-09-99"
+        },{
+            "id" : 1,
+            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
+            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 2,
@@ -121,61 +164,49 @@ if($page == '1'){
             "id" : 1,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 2,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 1,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 2,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 1,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 2,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 1,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         },{
             "id" : 2,
             "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
             "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 1,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 2,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 1,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
-            "info_time" : "2090-09-99"
-        },{
-            "id" : 2,
-            "info_msg" : "设置为 true 以在浏览器开发工具的性能/时间线面板中启用对组件初始化、编译、渲染和打补丁的性能追踪。只适用于开发模式和支持 performance.mark API 的浏览器上。",
-            "info_url" : "./article-list.html",
+            "info_author" : "的挥洒的",
             "info_time" : "2090-09-99"
         }]
     }';
